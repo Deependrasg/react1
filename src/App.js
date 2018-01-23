a6efb6b5d1871837959de50f1ec895f368849a28
Deependrasg <deependrasg@gmail.com>
2018-01-24 00:41:25 +0530

    Add Demo

--- src/App.js
+++ src/App.js
@@ -11,11 +11,52 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. hello deependra singh
         </p>
+        <h1> Im representing <State /> from here</h1>
+        <h1>{this.props.headerProp}</h1>
+        <h2>{this.props.contentProp}</h2>
       </div>
     );
   }
 }
-
+/////
 export default App;
+
+class State extends React.Component {
+   constructor(props) {
+      super(props);
+    
+      this.state = {
+         header: "Header from state...",
+         content: "Content from state..."
+      }
+   }
+   render() {
+      return (
+         <div>
+            <h1>{this.state.header}</h1>
+            <h2>{this.state.content}</h2>
+         </div>
+      );
+   }
+}
+
+
+export class First extends React.Component{
+  render(){
+    return (
+            <h1>Export Class First </h1>
+    );
+  }
+}
+// 
+export class Second extends React.Component{
+  render(){
+    return (
+            <h1>Export Class Second </h1>
+    );
+  }
+}
+// 
+// export class First;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
+// npm install react-images --save
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App,{First,Second} from './App';
+// import Show from './app1';
+import AppData,{Header,Content} from './app1';
+import App2 from './app2';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import registerServiceWorker from './registerServiceWorker';
+// import  from './app1';
+// import app1 from './app1';
+
+
+ 
+class MyComponent extends Component {
+ 
+  render() {
+    return (
+    	<div>
+      <img src={'https://images.unsplash.com/photo-1471101173712-b9884175254e?dpr=2&auto=format&w=1024&h=1024'} alt={'Picture'}/> 
+      <img src={'https://lh3.googleusercontent.com/U6_Snity8NZP3JHEv2oQqfiu-YuZNtaeJ8Z2SE_9Y-BOwluRCIKdYaN32oTBzPhaqf1-a72URAcyJf-mbEuIUy3a10DnuK5_uWhgIbCBeYZbu-VkMHHKPPsux2V9X7flofFfROhPDcF2y3iazj9t5oRVu_BD8UrxgywPIs8Zi7OMQh3_O5h1oNG-XkBcWp9zWNPb_Aa2z-i9VD61iqlOd3C141aMuLghKVlQE-29U7OK0M3_Uznhra4XtFgR6paPVwgMKpzI2dgrv2T24tM9EbndF9hp5yuPl5perN1g6VgD_xZnS2pyJ010tVcrcHgdFHohjC5E0o_npRgVYr0-BJItU_pRTBbhR5XTkI6mDve9Jy4i8xuc6xhh1ZsDDsaRPuFHQ8fRkrm4ARTizrBZ8qcQceqwqjNGc_WaRE4vcylx9vZC6nLQqITbXIgQcjHRK_gFjb4pGXa5Y6lF4Bv94NRqqYGXDot63Sh9qGtGQMUv5o0wgDNMEUr5oJmOSbiSmS-aZTM9RSPsmeyM0D-kRTbJ7B_Y3iJEZ_vzismCoBHzSZ66jxOpjmgm8Br1FLfitnqE6h1ulOitEzeYpIGiJanuFBWJRyx7szWFxJ4=w623-h399-no'} alt={'Picture'}/> 
+    </div>
+    );
+  } 
+}//
+
+
+
+ReactDOM.render(<MyComponent />, document.getElementById('root5'));
+ReactDOM.render(<App headerProp="{hello im HeaderPros }" contentProp="{hello in contentProp}" />, document.getElementById('root'));
 registerServiceWorker();
+ReactDOM.render(<Second />, document.getElementById('root1'));
+ReactDOM.render(<First />, document.getElementById('root2'));
+ReactDOM.render(<AppData />, document.getElementById('root3'));
+ReactDOM.render(<App2 />, document.getElementById('root4'));
+// ReactDOM.render(<Content />, document.getElementById('root5'));
+
+
+// registerServiceWorker();
